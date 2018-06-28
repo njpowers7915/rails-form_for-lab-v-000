640975f4e3af59bfd46c8d14e80a19be154859ec
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 11:31:34 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -26,7 +26,7 @@ class StudentsController < ApplicationController
     private
 
     def student_params
-        params.require(:student).permit(:title, :room_number)
+        params.require(:student).permit(:first_name, :last_name)
     end
 
 
