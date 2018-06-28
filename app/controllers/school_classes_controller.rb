300c5c867de34d6729bee6dbc07d544351b56def
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 11:12:23 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,13 +1,15 @@
 class SchoolClassesController < ApplicationController
+    def new
+        @school_class = SchoolClass.new
+    end
+
     def create
         @school_class = SchoolClass.new(params.require(:school_class).permit(:school_class))
         @school_class.save
         redirect_to school_class_path(@school_class)
     end
 
-    def new
-        @school_class = SchoolClass.new
-    end
+
 
     #def edit
     #    @school_class = SchoolClass.find(params[:id])
