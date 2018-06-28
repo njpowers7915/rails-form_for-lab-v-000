1da7f29c8a87170ddc84d7ba2375fbccec817212
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 11:28:41 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -9,21 +9,19 @@ class SchoolClassesController < ApplicationController
         redirect_to school_class_path(@school_class)
     end
 
-
-
-    #def edit
-    #    @school_class = SchoolClass.find(params[:id])
-    #end
-
     def show
         @school_class = SchoolClass.find(params[:id])
     end
 
-    #def update
-    #    @school_class = SchoolClass.find(params[:id])
-    #    @school_class.update(params.require(:school_class).permit(:school_class))
-    #    redirect_to school_class_path(@school_class)
-    #end
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(school_class_params)
+        redirect_to school_class_path(@school_class)
+    end
 
     private
 
