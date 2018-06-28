5e4846de9f6b741bfb1d227c204b40567374ee6e
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 11:30:32 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,33 @@
 class StudentsController < ApplicationController
+    def new
+        @student = Student.new
+    end
+
     def create
-        @student = Student.new(params.require(:student))
+        @student = Student.new(student_params)
         @student.save
         redirect_to student_path(@student)
     end
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        @student.update(school_class_params)
+        redirect_to school_class_path(@student)
+    end
+
+    private
+
+    def student_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+
+
 end
